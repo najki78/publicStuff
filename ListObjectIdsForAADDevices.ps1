f15fd6050c028e9f55c83c131e46df93850ede00
najki78 <lubos.nikolini@gmail.com>
2021-12-15 11:30:14 +0100

    Create ListObjectIdsForAADDevices.ps1

---
+++ ListObjectIdsForAADDevices.ps1
@@ -0,0 +1,31 @@
+﻿
+# lists ObjectID for the list of Azure AD devices
+
+
+#Get AzureAD Module
+$m = Get-Module -Name AzureAD -ListAvailable
+
+Write-Output $m
+
+if (-not $m)
+{
+    
+    Install-Module -Name AzureAD -AllowClobber -Force 
+   
+}
+
+Install-Module AzureAD
+Connect-AzureAD
+
+$outfile = "C:\temp\Outfile.csv"
+
+# input file "c:\temp\devices.csv" has a single column and a header "DeviceName" on the first line ...
+Import-Csv c:\temp\devices.csv | ForEach-Object {
+
+    (Get-AzureADDevice -SearchString $($_.DeviceName)).objectid | add-content -path $outfile
+    Write-Host "$($_.DeviceName)"
+
+}
+
+
+
