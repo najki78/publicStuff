ceb18768e8d54dcea9a280531c24f07a27a2812c
Ľuboš Nikolíni <48955715+najki78@users.noreply.github.com>
2021-12-27 18:02:46 +0100

    Create Connect-AipService and Get-AadrmConfiguration.ps1

---
+++ Connect-AipService and Get-AadrmConfiguration.ps1
@@ -0,0 +1,13 @@
+﻿
+Set-ExecutionPolicy -ExecutionPolicy Bypass 
+Get-ExecutionPolicy -List
+
+Import-Module PowerShellGet
+Install-Module -Name AIPService
+
+Connect-AipService
+
+Get-AadrmConfiguration 
+
+# Enterprise Roaming 
+# https://docs.microsoft.com/en-us/azure/active-directory/devices/enterprise-state-roaming-faqs
