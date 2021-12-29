4a5aa9ce5aa82c0c7f976dd2f0a5c1feced07321
Ľuboš Nikolíni <48955715+najki78@users.noreply.github.com>
2021-12-29 18:51:13 +0100

    Create Intune-SyncSelectedDevices.ps1

---
+++ PowerShell-Intune/Intune-SyncSelectedDevices.ps1
@@ -0,0 +1,28 @@
+﻿
+
+# Sync selected Devices
+# thanks to 
+
+
+Connect-MSGraph
+
+
+#$DevicesToSync = Get-IntuneManagedDevice | Get-MSGraphAllPages | where-object {$_.managementAgent -eq 'mdm'}
+
+
+#$IPs = (Get-Content IP.csv)[0].split(",")
+#foreach ( $IP in $IPs){    echo $IP}
+
+
+$DevicesToSync = Get-IntuneManagedDevice -Filter "contains(deviceName,'M5332696')" 
+#| select serialnumber, devicename, userDisplayName, userPrincipalName, id, userId, azureADDeviceId, managedDeviceOwnerType, model, manufacturer
+
+
+Foreach ($Device in $DevicesToSync)
+{
+ 
+Invoke-IntuneManagedDeviceSyncDevice -managedDeviceId $Device.managedDeviceId
+Write-Host "Sending Sync request to Device with Name $($Device.deviceName)" -ForegroundColor Green
+ 
+}
+
