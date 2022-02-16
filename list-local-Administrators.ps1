9a105148496f58aa6655b80b30ed1bb21937e5ef
Ľuboš Nikolíni <48955715+najki78@users.noreply.github.com>
2022-02-16 11:07:17 +0100

    Update list-local-Administrators.ps1

--- list-local-Administrators.ps1
+++ list-local-Administrators.ps1
@@ -3,6 +3,61 @@
 
 # https://github.com/PowerShell/PowerShell/issues/2996
 
+
+
+function Convert-AzureAdSidToObjectId {
+<#
+.SYNOPSIS
+Convert a Azure AD SID to Object ID
+ 
+.DESCRIPTION
+Converts an Azure AD SID to Object ID.
+Author: Oliver Kieselbach (oliverkieselbach.com)
+The script is provided "AS IS" with no warranties.
+ 
+.PARAMETER ObjectID
+The SID to convert
+#>
+
+    param([String] $Sid)
+
+    $text = $sid.Replace('S-1-12-1-', '')
+    $array = [UInt32[]]$text.Split('-')
+
+    $bytes = New-Object 'Byte[]' 16
+    [Buffer]::BlockCopy($array, 0, $bytes, 0, 16)
+    [Guid]$guid = $bytes
+
+    return $guid
+}
+
+
+function Convert-AzureAdObjectIdToSid {
+<#
+.SYNOPSIS
+Convert an Azure AD Object ID to SID
+ 
+.DESCRIPTION
+Converts an Azure AD Object ID to a SID.
+Author: Oliver Kieselbach (oliverkieselbach.com)
+The script is provided "AS IS" with no warranties.
+ 
+.PARAMETER ObjectID
+The Object ID to convert
+#>
+
+    param([String] $ObjectId)
+
+    $bytes = [Guid]::Parse($ObjectId).ToByteArray()
+    $array = New-Object 'UInt32[]' 4
+
+    [Buffer]::BlockCopy($bytes, 0, $array, 0, 16)
+    $sid = "S-1-12-1-$array".Replace(' ', '-')
+
+    return $sid
+}
+
+
 <#
 Get-LocalGroupMember -Group "Administrators"
 
@@ -54,4 +109,42 @@ $administrators = @(
 
 $administrators = $administrators -replace "WinNT://",""
 
-$administrators
+#$administrators 
+
+Connect-AzureAD
+
+foreach ($adminSID in $administrators) {
+
+    if ($adminSID -like "S-1-*") {
+        $objectId = Convert-AzureAdSidToObjectId -Sid $adminSID
+        #$adminSID
+        #$objectId 
+        (Get-AzureADObjectByObjectId -ObjectIds $objectId -ErrorAction SilentlyContinue).DisplayName
+     } else {
+        #Write-Host "XXX"
+        $adminSID 
+     }
+}
+
+
+
+
+<#
+$objectId = "a18b5f0b-375d-48ac-ab7f-9a952042df35"
+$sid = Convert-AzureAdObjectIdToSid -ObjectId $objectId
+Write-Output $sid
+#>
+
+<# 
+
+$sid = "S-1-12-1-3355307493-1154222592-1435707025-2509201134"
+$objectId = Convert-AzureAdSidToObjectId -Sid $sid
+Write-Output $objectId
+
+# Output:
+
+# Guid
+# ----
+# 73d664e4-0886-4a73-b745-c694da45ddb4
+
+#>
