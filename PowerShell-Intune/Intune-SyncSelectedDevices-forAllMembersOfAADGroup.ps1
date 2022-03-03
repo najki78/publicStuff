6b930924308572009b607b15ef0c77d85ffd9ba8
Ľuboš Nikolíni <48955715+najki78@users.noreply.github.com>
2022-03-03 14:43:41 +0100

    Create Intune-SyncSelectedDevices-forAllMembersOfAADGroup.ps1

---
+++ PowerShell-Intune/Intune-SyncSelectedDevices-forAllMembersOfAADGroup.ps1
@@ -0,0 +1,32 @@
+﻿cls
+
+if(!(Connect-MSGraph)){ Connect-MSGraph }
+
+try {
+
+    # Which AAD group do we want to check against
+    $groupName = "Intune-Devices-EMEA-All"
+ 
+    #$Groups = Get-AADGroup | Get-MSGraphAllPages
+    $Group = Get-AADGroup -Filter "displayname eq '$GroupName'"
+    #$Group.id = Azure ObjectId 
+    #$Group.securityIdentifier = SID
+
+    Write-host "AAD Group Name: $($Group.displayName)" -ForegroundColor Green
+ 
+    $listOfDevices = Get-AzureADGroupMember -ObjectId $Group.id -All $true # -Top 5 
+    # ObjectId, DeviceId (=azureADDeviceId),DisplayName
+
+    foreach($AzureDeviceObj in $listOfDevices) {
+
+        try {
+            $IntuneDeviceObj = Get-IntuneManagedDevice -Filter “azureADDeviceId eq '$($AzureDeviceObj.DeviceId)'”
+            #azureADDeviceId, id (Intune DeviceId) 
+            if($IntuneDeviceObj -ne $null) { Invoke-IntuneManagedDeviceSyncDevice -managedDeviceId $IntuneDeviceObj.id; Write-Host "Sync sent to $($AzureDeviceObj.DisplayName) - $($IntuneDeviceObj.id)" }
+        } # inner 'try' block
+        catch { Write-Host -Message $_ }
+        
+    }
+    
+} # main 'try' block
+catch { Write-Host -Message $_ }
