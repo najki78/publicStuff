ecaac1519ba8cdeda11ee87669c8f6d83d1cb305
Ľuboš Nikolíni <48955715+najki78@users.noreply.github.com>
2022-03-03 14:42:16 +0100

    Create terminateAllScreenSaverProcesses.ps1

---
+++ terminateAllScreenSaverProcesses.ps1
@@ -0,0 +1,10 @@
+﻿
+# 2022-02-17 Lubos
+
+# terminate all screen saver processes
+
+#foreach ($screenSaver in (Get-Process | where ProcessName -like "*.scr")) { Stop-Process -Id $screenSaver.Id }
+
+#Get-Process | where ProcessName -like "*.scr" | Select Id | Stop-Process
+
+Stop-Process -Name "*.scr" -Force -PassThru
