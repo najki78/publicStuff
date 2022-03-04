a8c72cbd16fc2b016091fa9eedf336cbe177244f
Ľuboš Nikolíni <48955715+najki78@users.noreply.github.com>
2022-03-04 15:53:19 +0100

    Update Get_all_assigned_Intune_policies_and_apps_per_Azure_AD_group.ps1

--- PowerShell-Intune/Get_all_assigned_Intune_policies_and_apps_per_Azure_AD_group.ps1
+++ PowerShell-Intune/Get_all_assigned_Intune_policies_and_apps_per_Azure_AD_group.ps1
@@ -44,7 +44,7 @@ if(!(Connect-MSGraph)){
 }
  
 # Which AAD group do we want to check against
-$groupName = "Intune-ProactiveRemediation-ReportNetworkConnections"
+$groupName = "Intune-Agent-Windows-Zscaler-ShopfloorNewApproach"
  
 # All AAD groups ... $Groups = Get-AADGroup | Get-MSGraphAllPages
 # All Intune groups in AAD ... e.g. $Groups = Get-AADGroup | Get-MSGraphAllPages | Where {($_.displayName -like “NL-*” -or $_.displayName -like “*Intune*”)}
