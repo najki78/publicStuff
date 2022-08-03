058d9e1e7b7655ed38646a508bfedd1542f811d7
Ľuboš Nikolíni <lubos.nikolini@gmail.com>
2022-08-03 14:28:11 +0200

    Update runAutopilotDiagnostics.ps1

--- PowerShell-Intune/runAutopilotDiagnostics.ps1
+++ PowerShell-Intune/runAutopilotDiagnostics.ps1
@@ -5,6 +5,91 @@
 
 # mdmdiagnosticstool.exe -area Autopilot -cab c:\autopilot.cab
 
+#mdmdiagnosticstool.exe -area Autopilot;DeviceEnrollment;DeviceProvisioning;TPM -cab c:\temp\autopilot.cab
+
+<#
+
+Usage1: MDMDiagnosticsTool.exe -out <output folder path>
+      * Output MDM diagnostics info only to given folder path specified in -out parameter.
+      eg: MDMDiagnosticsTool.exe -out c:\temp\outputfolder
+
+  Usage2: MDMDiagnosticsTool.exe -area <area name(s)> -cab <output cab file path>
+      * Collect predefined area logs and create a log cab to given cab file.
+      * Supported area name example:
+          Autopilot
+          DeviceProvisioning
+          Tpm
+      * It also supports multiple areas, separated by ';', example:
+          Autopilot;DeviceEnrollment;Tpm
+      * Please find all possible areas in registry under:
+          HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MdmDiagnostics\Area
+      eg: MDMDiagnosticsTool.exe -area Autopilot;Tpm -cab c:\temp\AutopilotDiag.cab
+  Usage3: MDMDiagnosticsTool.exe -area <area name(s)> -zip <output zip file path>
+      * Collect predefined area logs and create a log zip to given zip file. Areas supported are the same as Usage2 for creating cab
+  Usage4: MDMDiagnosticsTool.exe -xml <xml file of information to gather> -zip <output zip file path> -server <MDM Server to alert>
+      * Collect information specified in the xml and create a log zip to given zip file.
+
+#>
+
+<#
+
+Example of the CAB file content:
+
+    AgentExecutor-20220722-141658.log
+    AgentExecutor-20220728-172654.log
+    AgentExecutor.log
+    AutopilotConciergeFile.json
+    AutopilotDDSZTDFile.json
+    CertReq_enrollaik_Output.txt
+    CertUtil_tpminfo_Output.txt
+    ClientHealth.log
+    DeviceHash_MB401346.csv
+    DiagnosticLogCSP_Collector_Autopilot_2021_12_22_4_16_20.etl
+    DiagnosticLogCSP_Collector_Autopilot_2021_12_22_4_19_19.etl
+    DiagnosticLogCSP_Collector_Autopilot_2021_12_22_4_39_26.etl
+    DiagnosticLogCSP_Collector_Autopilot_2021_12_22_4_42_18.etl
+    DiagnosticLogCSP_Collector_DeviceEnrollment_2021_12_22_4_31_55.etl
+    DiagnosticLogCSP_Collector_DeviceEnrollment_2021_12_22_4_57_5.etl
+    DiagnosticLogCSP_Collector_DeviceProvisioning_2022_2_24_16_42_27.etl.merged
+    DiagnosticLogCSP_Collector_DeviceProvisioning_2022_7_26_21_6_26.etl
+    DiagnosticLogCSP_Collector_DeviceProvisioning_2022_7_29_10_46_56.etl
+    DiagnosticLogCSP_Collector_DeviceProvisioning_2022_8_1_12_32_21.etl
+    DiagnosticLogCSP_Collector_DeviceProvisioning_2022_8_3_11_9_27.etl
+    IntuneManagementExtension-20220803-110145.log
+    IntuneManagementExtension-20220803-134049.log
+    IntuneManagementExtension.log
+    LicensingDiag.cab
+    LicensingDiag_Output.txt
+    MDMDiagHtmlReport.html
+    MdmDiagLogMetadata.json
+    MDMDiagReport.xml
+    MdmDiagReport_RegistryDump.reg
+    MdmLogCollectorFootPrint.txt
+    microsoft-windows-aad-operational.evtx
+    microsoft-windows-appxdeploymentserver-operational.evtx
+    microsoft-windows-assignedaccess-admin.evtx
+    microsoft-windows-assignedaccess-operational.evtx
+    microsoft-windows-assignedaccessbroker-admin.evtx
+    microsoft-windows-assignedaccessbroker-operational.evtx
+    microsoft-windows-crypto-ncrypt-operational.evtx
+    microsoft-windows-devicemanagement-enterprise-diagnostics-provider-admin.evtx
+    microsoft-windows-devicemanagement-enterprise-diagnostics-provider-debug.evtx
+    microsoft-windows-devicemanagement-enterprise-diagnostics-provider-operational.evtx
+    microsoft-windows-moderndeployment-diagnostics-provider-autopilot.evtx
+    microsoft-windows-moderndeployment-diagnostics-provider-managementservice.evtx
+    microsoft-windows-provisioning-diagnostics-provider-admin.evtx
+    microsoft-windows-shell-core-operational.evtx
+    microsoft-windows-user device registration-admin.evtx
+    Sensor-20220624-152543.log
+    Sensor-20220720-002439.log
+    Sensor.log
+    setupact.log
+    TpmHliInfo_Output.txt
+
+#>
+
+
+
 <#
 SYNTAX
     C:\Program Files\WindowsPowerShell\Scripts\Get-AutopilotDiagnostics.ps1 [[-CABFile] <String>] [[-ZIPFile] <String>] [-Online] [-AllSessions] [-ShowPolicies] [<CommonParameters>]
