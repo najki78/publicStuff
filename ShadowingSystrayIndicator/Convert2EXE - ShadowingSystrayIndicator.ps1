7eedfe1dabea28f5a65570d3592d71db46c0046e
Ľuboš Nikolíni <lubos@nikolini.eu>
2024-12-27 12:15:11 +0100

    Update Convert2EXE - ShadowingSystrayIndicator.ps1

--- ShadowingSystrayIndicator/Convert2EXE - ShadowingSystrayIndicator.ps1
+++ ShadowingSystrayIndicator/Convert2EXE - ShadowingSystrayIndicator.ps1
@@ -1,2 +1,2 @@
 ﻿Install-Module ps2exe
-Invoke-ps2exe "ShadowingSystrayIndicator.ps1" -title "Remote Desktop Services Systray Indicator" -verbose -noConsole # -requireAdmin -DPIAware -credentialGUI -noConsole 
+Invoke-ps2exe ".\ShadowingSystrayIndicator.ps1" -title "Remote Desktop Services Systray Indicator" -verbose -noConsole # -requireAdmin -DPIAware -credentialGUI -noConsole 
